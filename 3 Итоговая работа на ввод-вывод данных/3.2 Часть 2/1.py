a4a294380979cf8532ecb5356cda3670884b9d14
RAX3R <raxer93@mail.ru>
2020-11-28 12:47:40 +0300

    3.2 - 1

---
+++ 3 Итоговая работа на ввод-вывод данных/3.2 Часть 2/1.py
@@ -0,0 +1,18 @@
+print("*" * 17)
+print("*" + " " * 15 + "*")
+print("*" + " " * 15 + "*")
+print("*" * 17)
+
+"""
+for i in range(4):
+    line = ""
+    for j in range(17):
+        if i == 0 or i == 3:
+            line += "*"
+        else:
+            if j == 0 or j == 16:
+                line += "*"
+            else:
+                line += " "
+    print(line)
+"""
