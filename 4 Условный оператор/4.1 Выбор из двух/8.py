c1b14187a86ea1ca8404d0c7f2f9447e8a7219bf
RAX3R <raxer93@mail.ru>
2020-12-04 22:24:05 +0300

    4.1 - 8

---
+++ 4 Условный оператор/4.1 Выбор из двух/8.py
@@ -0,0 +1,10 @@
+age = int(input())
+
+if age <= 13:
+    print("детство")
+elif 14 <= age <= 24:
+    print("молодость")
+elif 25 <= age <= 59:
+    print("зрелость")
+elif age >= 60:
+    print("старость")
