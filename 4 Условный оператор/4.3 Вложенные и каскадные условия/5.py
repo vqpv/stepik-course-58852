556131704606fafe7d9d36373c89ab697a2a2698
RAX3R <raxer93@mail.ru>
2020-12-13 16:13:45 +0300

    4.3 - 5

---
+++ 4 Условный оператор/4.3 Вложенные и каскадные условия/5.py
@@ -0,0 +1,10 @@
+weight = int(input())
+
+if weight < 60:
+    print("Легкий вес")
+
+elif 60 <= weight < 64:
+    print("Первый полусредний вес")
+
+elif 64 <= weight < 69:
+    print("Полусредний вес")
