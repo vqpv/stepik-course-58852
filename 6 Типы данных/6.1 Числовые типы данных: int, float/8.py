dfcd3a3e58a9acfb83fd9e5fc1c4fbdee21e2069
RAX3R <raxer93@mail.ru>
2020-12-24 16:45:23 +0300

    6.1 - 8

---
+++ 6 Типы данных/6.1 Числовые типы данных: int, float/8.py
@@ -0,0 +1,4 @@
+num1, num2, num3, num4, num5 = int(input()), int(input()), int(input()), int(input()),int(input())
+
+print("Наименьшее число =", min(num1, num2, num3, num4, num5))
+print("Наибольшее число =", max(num1, num2, num3, num4, num5))
