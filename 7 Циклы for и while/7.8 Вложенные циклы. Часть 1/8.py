0339b7bfde66912c0361f1ed650414a0257da393
RAX3R <45417916+vqpv@users.noreply.github.com>
2021-02-04 20:49:06 +0300

    7.8 - 8

---
+++ 7 Циклы for и while/7.8 Вложенные циклы. Часть 1/8.py
@@ -0,0 +1,7 @@
+for a in range(1, 151):
+    for b in range(a + 1, 151):
+        for c in range(b + 1, 151):
+            for d in range(c + 1, 151):
+                e = int(((a ** 5) + (b ** 5) + (c ** 5) + (d ** 5)) ** 0.2)
+                if e ** 5 == int((a ** 5) + (b ** 5) + (c ** 5) + (d ** 5)):
+                    print(a + b + c + d + e)
