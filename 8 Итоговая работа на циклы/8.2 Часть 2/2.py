7cb9be1dcbc91557c5cead0a19e04b3565dd9dac
RAX3R <45417916+vqpv@users.noreply.github.com>
2021-02-11 22:44:28 +0300

    8.2 - 2

---
+++ 8 Итоговая работа на циклы/8.2 Часть 2/2.py
@@ -0,0 +1,16 @@
+n = 8
+count = 0
+maximum = -1000
+
+for _ in range(1, n + 1):
+    x = int(input())
+    if x % 4 == 0:
+        count += 1
+        if x > maximum:
+            maximum = x
+
+if count > 0:
+    print(count)
+    print(maximum)
+else:
+    print('NO')
