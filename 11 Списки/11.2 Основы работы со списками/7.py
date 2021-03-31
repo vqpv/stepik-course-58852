ff8367c96613fd14cc71e10eaae721951a687c64
RAX3R <45417916+vqpv@users.noreply.github.com>
2021-03-31 23:06:03 +0300

    11.2 - 7

---
+++ 11 Списки/11.2 Основы работы со списками/7.py
@@ -0,0 +1,4 @@
+languages = ['Chinese', 'Spanish', 'English', 'Hindi', 'Arabic', 'Bengali', 'Portuguese', 'Russian', 'Japanese', 'Lahnda']
+
+"""print(list(reversed(languages)))"""
+print(languages[::-1])
