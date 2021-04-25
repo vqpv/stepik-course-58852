aa6789e335af62ca30407913c37ee9ea3e3febb4
RAX3R <45417916+vqpv@users.noreply.github.com>
2021-04-25 21:37:15 +0300

    11.7 - 3

---
+++ 11 Списки/11.7 Списочные выражения/3.py
@@ -0,0 +1,5 @@
+keywords = ['False', 'True', 'None', 'and', 'with', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'try', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'while', 'yield']
+
+new_keywords = [word for word in keywords if len(word) >= 5]
+
+print(new_keywords)
